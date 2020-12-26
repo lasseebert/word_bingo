5a5911f28b183b27953d9ca635554ea3c43909c9
Lasse Skindstad Ebert <lasse@lasseebert.dk>
2020-12-26 22:42:18 +0100

    Dialyzer

--- mix.exs
+++ mix.exs
@@ -21,7 +21,9 @@ defmodule WordBingo.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
+      # Linting
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false}
     ]
   end
 end
